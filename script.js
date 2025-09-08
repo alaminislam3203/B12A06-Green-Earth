338552f677e325f67627c780fcea0d239f3045a1
alaminislam3203 <alaminislam3203@gmail.com>
2025-09-08 19:46:00 +0600

    Show Plants by Selected Category java script  added

--- script.js
+++ script.js
@@ -108,4 +108,42 @@ const loadPlantsByCategory = async(id) => {
     const url = `https://openapi.programming-hero.com/api/category/${id}`;
     const res = await fetch(url);
     const data = await res.json();
-    showPlantsByCategory(data.plants, id);
+  showPlantsByCategory(data.plants, id);
+  
+
+
+  
+// Show Plants by Selected Category
+const showPlantsByCategory = (plants, id) => {
+    removeActive();
+    const clickedCategory = document.getElementById(`tree-${id}`);
+    clickedCategory.classList.add("bg-[#15803d]", "text-white");
+
+    plantCards.innerHTML = "";
+    plants.forEach(plant => {
+        plantCards.innerHTML += `
+            <div class="card bg-base-100 w-full shadow-sm">
+                <figure class="px-4 pt-4">
+                    <img src="${plant.image}" alt="${plant.name}"
+                        class="rounded-xl w-full h-50 object-cover" />
+                </figure>
+                <div class="card-body pb-4">
+                    <h2 class="card-title cursor-pointer" 
+                        onclick="loadPlantDetail('${plant.id}')">${plant.name}</h2>
+                    <p>${plant.description}</p>
+                    <div class="flex justify-between items-center">
+                        <div class="rounded-full bg-[#dcfce7] text-[#15803d] py-1 px-3 font-semibold">${plant.category}</div>
+                        <div class="font-semibold">৳${plant.price}</div>
+                    </div>
+                </div>
+                <div class="pb-4 px-4">
+                    <button class="btn bg-[#15803d] btn-block rounded-full text-white 
+                        hover:scale-105 transition-transform duration-100 ease-in-out cursor-pointer">
+                        Add to Cart
+                    </button>
+                </div>
+            </div>
+        `;
+    });
+    manageLoader(false);
+}
